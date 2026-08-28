d504268928264d4fb907b61c17f690c2d8bb12d6
crang177 <1771856179@qq.com>
2026-08-28 14:05:56 +0800

    MyBatis-Plus----service 接口继承 IService 接口。实现类要实现接口并且继承 IserviceImpl<>类

---
+++ src/main/java/cd/cc/service/IUserService.java
@@ -0,0 +1,7 @@
+package cd.cc.service;
+
+import cd.cc.domain.po.User;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+public interface IUserService extends IService<User>  {
+}
---
+++ src/main/java/cd/cc/service/Impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package cd.cc.service.Impl;
+
+import cd.cc.domain.po.User;
+import cd.cc.mapper.UserMapper;
+import cd.cc.service.IUserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper,User> implements IUserService {
+
+}
